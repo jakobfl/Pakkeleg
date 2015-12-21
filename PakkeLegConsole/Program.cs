08e281df748d8ac4db15eaaf6d43e0e8a9483893
jakob Larsen <jakobfl@live.dk>
2015-12-21 14:05:46 +0100

    created pakkeleg

---
+++ PakkeLegConsole/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PakkeLegConsole
+{
+    class Program
+    {
+        static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            Terningslag();
+
+            Terningslag();
+
+            Terningslag();
+
+            Terningslag();
+
+            Terningslag();
+
+            Terningslag();
+
+            Console.ReadLine();
+        }
+
+        static void Terningslag()
+        {
+            Console.WriteLine("Tryk på en enter");
+            Console.ReadLine();
+            int tal = rnd.Next(1, 7);
+            Console.WriteLine(tal);
+            if (tal == 6)
+            {
+                Console.WriteLine("PAKKE TIL DIG!!");
+            }
+            else
+            {
+                Console.WriteLine("næste persons tur");
+            }
+ 
+        }
+    }
+}
